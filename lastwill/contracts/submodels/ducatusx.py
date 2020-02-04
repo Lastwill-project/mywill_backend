b56bd1168eedf1b439f659c9e51c027a466fe476
Egor14 <egorgrekhnev@mail.ru>
2020-02-04 11:35:14 +0300

    add decorators to ducatusx model classes

--- lastwill/contracts/submodels/ducatusx.py
+++ lastwill/contracts/submodels/ducatusx.py
@@ -1,4 +1,4 @@
-from .common import CommonDetails
+from lastwill.contracts.submodels.common import *
 from .lastwill import ContractDetailsLastwill
 from .lostkey import ContractDetailsLostKey
 from .deffered import ContractDetailsDelayedPayment
@@ -9,26 +9,34 @@ from .investment_pool import ContractDetailsInvestmentPool
 from .lostkey import ContractDetailsLostKeyTokens
 
 
+@contract_details('DUCATUSX Will contract')
 class ContractDetailsDUCATUSXLastwill(ContractDetailsLastwill):
     pass
 
+@contract_details('DUCATUSX Wallet contract (lost key)')
 class ContractDetailsDUCATUSXLostKey(ContractDetailsLostKey):
     pass
 
+@contract_details('DUCATUSX Deferred payment contract')
 class ContractDetailsDUCATUSXDelayedPayment(ContractDetailsDelayedPayment):
     pass
 
+@contract_details('DUCATUSX MyWish ICO')
 class ContractDetailsDUCATUSXICO(ContractDetailsICO):
     pass
 
+@contract_details('DUCATUSX Token contract')
 class ContractDetailsDUCATUSXToken(ContractDetailsToken):
     pass
 
+@contract_details('DUCATUSX Airdrop')
 class ContractDetailsDUCATUSXAirdrop(ContractDetailsAirdrop):
     pass
 
+@contract_details('DUCATUSX Investment Pool')
 class ContractDetailsDUCATUSXInvestmentPool(ContractDetailsInvestmentPool):
     pass
 
+@contract_details('DUCATUSX Wallet contract (lost key)')
 class ContractDetailsDUCATUSXLostKeyTokens(ContractDetailsLostKeyTokens):
     pass
